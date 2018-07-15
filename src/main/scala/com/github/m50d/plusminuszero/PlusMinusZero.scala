b1f6a9c971c2e370c9913184cde374b06c85f6c0
Michael Donaghy <md401@srcf.ucam.org>
2018-07-15 10:41:21 +0100

    Expose thing to javascript

--- src/main/scala/com/github/m50d/plusminuszero/PlusMinusZero.scala
+++ src/main/scala/com/github/m50d/plusminuszero/PlusMinusZero.scala
@@ -1,6 +1,10 @@
 package com.github.m50d.plusminuszero
 
+import scala.scalajs.js.annotation._
+
+@JSExportTopLevel("plusMinusZero.PlusMinusZero")
 object PlusMinusZero {
+  @JSExport
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
