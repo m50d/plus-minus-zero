e5a968aa6830a5496abb8dd114ce3f8ad31254bd
Michael Donaghy <md401@srcf.ucam.org>
2019-07-30 11:42:15 +0100

    Add import EMA button

--- src/main/scala/com/github/m50d/plusminuszero/PlusMinusZero.scala
+++ src/main/scala/com/github/m50d/plusminuszero/PlusMinusZero.scala
@@ -9,10 +9,9 @@ import monix.reactive.Observable
 import monix.reactive.subjects.Var
 import monix.execution.Scheduler.Implicits.global
 
-@JSExportTopLevel("plusMinusZero.PlusMinusZero")
-object PlusMinusZero {
-  @JSExport
-  def main(args: Array[String]): Unit = {
+@JSExportTopLevel("plusMinusZero.PlusMinusZero") object PlusMinusZero {
+  @JSExport def main(args: Array[String]): Unit = {
+    val emaId = Var(None): Var[Option[String]]
     val toAdd = Var(None): Var[Option[TournamentResult]]
     val added = toAdd.scan(Vector[TournamentResult]())(_ ++ _)
     val toRemove = Var(None): Var[Option[TournamentResult]]
@@ -21,7 +20,8 @@ object PlusMinusZero {
 
     val explanation = {
       val el: html.Paragraph = document.createElement("p").asInstanceOf[html.Paragraph]
-      val text = document.createTextNode("""
+      val text = document.createTextNode(
+        """
 In "weight", enter MERS weight for tournaments in the past year and MERS weight/2
 for tournaments between 1 and 2 years ago.
 
@@ -31,10 +31,14 @@ and around 0 for last.
       el.appendChild(text)
       Owlet(Observable(List(el)), Var(()))
     }
+    val emaIdEntry = label(string("emaid", ""), "EMA ID")
+    val importEma = (emaIdEntry, button("import", false, true)).mapN((id, pressed) =>
+      emaId := (if(pressed) Some(id) else None)
+    )
 
     val resultEntry = div(TournamentResult.tournamentResult, Var(Seq("result-entry")))
-    val addNewResult = (resultEntry, button("add", false, true)).mapN((
-      (entry, pressed) => toAdd := (if (pressed) Some(entry) else None)))
+    val addNewResult = (resultEntry, button("add", false, true)).mapN(((entry, pressed) =>
+      toAdd := (if (pressed) Some(entry) else None)))
 
     val rankUi = {
       val sink = Var(())
@@ -49,8 +53,7 @@ and around 0 for last.
           val tn = document.createTextNode(result.toString)
           rel.className = "tournament-result"
           rel.appendChild(tn)
-          val removeButton =
-          document.createElement("button").asInstanceOf[html.Button]
+          val removeButton = document.createElement("button").asInstanceOf[html.Button]
           removeButton.appendChild(document.createTextNode("×"))
           removeButton.onclick = { _ =>
             toRemove := Some(result)
@@ -65,6 +68,6 @@ and around 0 for last.
       }
       Owlet(Observable(List(el)), sink)
     }
-    render(explanation *> addNewResult *> rankUi, "#app")
+    render(explanation *> importEma *> addNewResult *> rankUi, "#app")
   }
 }
